be436f66a295ff15c5e8cad3260c4e529352d3ca
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-07-01 11:02:56 +0545

    ssadqewd

--- DOTNET/Day1/Program.cs
+++ DOTNET/Day1/Program.cs
@@ -1,12 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Mater!");
 int a = 3;
 
 var b = a + a;
 Console.WriteLine(b);
 
-int add(int a, int b)
+static int add(int a, int b)
 {
     return a + b;
 }
 
 Console.WriteLine(add(5, 5));
+
+namespace hen
+{
+
+    public class Wee
+    {
+        public void Suuu()
+        {
+            Console.WriteLine("aaaaaaaaaaawwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+        }
+    }
+}
+
+namespace awww
+{
+
+    internal class Rrra
+    {
+        static void Main(string[] args)
+        {
+            hen.Wee arraarra = new hen.Wee();
+            arraarra.Suuu();
+        }
+    }
+}
