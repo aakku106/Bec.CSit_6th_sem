a25b693ebbc3d64874a862d35bf51c995417a93e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-07-03 10:59:30 +0545

    different types fo cronstructor

--- DOTNET/Day1/Program.cs
+++ DOTNET/Day1/Program.cs
@@ -14,6 +14,23 @@ Console.WriteLine(add(5, 5));
 
 hen.Wee arraarra = new hen.Wee("Master CCN");
 
+// DAY2 
+// ceating objects
+day2.Laptop mac = new day2.Laptop();
+day2.Laptop mac3 = new day2.Laptop("Mac book air m4", 16, "adsm22dds");
+// giving value
+mac.ramSize = 8;
+mac.serialNum = "aakakaskdnjsf38009824";
+mac.manufacturer = "Apple inc.";
+
+
+// calling methods
+
+day2.Laptop mac2 = new day2.Laptop(mac);
+mac.DispplayDetails();
+mac2.DispplayDetails();
+mac3.DispplayDetails();
+
 namespace hen
 {
 
@@ -39,3 +56,58 @@ namespace hen
         }
     }
 }
+
+
+namespace day2
+
+//create a class laptop that includes:
+// 1. Fields:
+// - manufacturer (public; string)
+// - ramSize(public; Int)
+// -serieal numb (public; string)
+
+//2. methods:
+//showSpace()
+//DisplayDetails()
+//ShowManufacturer()
+{
+    public class Laptop
+    {
+        public string manufacturer;
+        public int ramSize;
+        public string serialNum;
+
+        public Laptop()
+        {
+            manufacturer = "";
+            ramSize = 0;
+            serialNum = "";
+        }
+        public Laptop(string m, int r, string s)
+        {
+            manufacturer = m;
+            ramSize = r;
+            serialNum = s;
+        }
+        public Laptop(Laptop existingObj)
+        {
+            serialNum = existingObj.serialNum;
+            ramSize = existingObj.ramSize;
+            manufacturer = existingObj.manufacturer;
+        }
+
+        public void showSpace()
+        {
+            Console.WriteLine("Ram size:\t", ramSize);
+        }
+        public void DispplayDetails()
+        {
+            Console.WriteLine("\nManufuctur:\t" + manufacturer + "\nram size:\t" + ramSize + "\nserial number:\t" + serialNum);
+        }
+        public void Showmanufacturer()
+        {
+            Console.WriteLine("Manufuctur:", manufacturer);
+        }
+    }
+}
+
