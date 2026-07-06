4567b18b196e42cc276982539edba14f668eb9fc
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-07-06 10:26:25 +0545

    day3

--- DOTNET/Day1/Program.cs
+++ DOTNET/Day1/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine(add(5, 5));
 
 hen.Wee arraarra = new hen.Wee("Master CCN");
 
-// ------- DAY2 --------------
+// DAY2 
 // ceating objects
 day2.Laptop mac = new day2.Laptop();
 day2.Laptop mac3 = new day2.Laptop("Mac book air m4", 16, "adsm22dds");
@@ -134,4 +134,9 @@ namespace day2
         }
 
     }
+}
+
+namespace day3
+{
+
 }
