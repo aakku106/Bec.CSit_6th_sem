d220d383ab67710ce548a11f74e6d506eb37b59d
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-07-30 10:05:44 +0545

    Add Login functionality and update layout with navigation link

--- DOTNET/MVC/learnMVC/Controllers/HomeController.cs
+++ DOTNET/MVC/learnMVC/Controllers/HomeController.cs
@@ -19,6 +19,30 @@ public class HomeController : Controller
     {
         return View();
     }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        // Hardcoded credentials, just for learning purposes.
+        if (username == "admin" && password == "1234")
+        {
+            ViewBag.Message = "Login successful!";
+            ViewBag.Success = true;
+        }
+        else
+        {
+            ViewBag.Message = "Invalid username or password.";
+            ViewBag.Success = false;
+        }
+
+        return View();
+    }
+   
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
